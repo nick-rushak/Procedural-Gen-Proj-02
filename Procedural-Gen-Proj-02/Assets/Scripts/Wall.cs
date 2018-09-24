782d4c1cefa0b1c53ad3bfc1465f7846fc157d41
unknown <njrushak@protonmail.com>
2018-09-23 20:16:09 -0700

    adding random items to chests

--- Procedural-Gen-Proj-02/Assets/Scripts/Wall.cs
+++ Procedural-Gen-Proj-02/Assets/Scripts/Wall.cs
@@ -4,31 +4,25 @@ using Random = UnityEngine.Random;
 
 public class Wall : MonoBehaviour
 {
-	public Sprite dmgSprite;					//Alternate sprite to display after Wall has been attacked by player.
-	public int hp = 3;							//hit points for the wall.
+    public Sprite dmgSprite;
+    public int hp = 3;
     public GameObject[] foodTiles;
 
-	private SpriteRenderer spriteRenderer;		//Store a component reference to the attached SpriteRenderer.
+    private SpriteRenderer spriteRenderer;
 
-	void Awake ()
-	{
-		//Get a component reference to the SpriteRenderer.
-		spriteRenderer = GetComponent<SpriteRenderer> ();
-	}
-	
-	
-	//DamageWall is called when the player attacks a wall.
-	public void DamageWall (int loss)
-	{
-		
-		//Set spriteRenderer to the damaged wall sprite.
-		spriteRenderer.sprite = dmgSprite;
-		
-		//Subtract loss from hit point total.
-		hp -= loss;
-		
-		//If hit points are less than or equal to zero:
-		if(hp <= 0)
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void DamageWall(int loss)
+    {
+
+        spriteRenderer.sprite = dmgSprite;
+        hp -= loss;
+
+        if (hp <= 0)
+        {
             if (Random.Range(0, 5) == 1)
             {
                 GameObject toInstantiate = foodTiles[Random.Range(0, foodTiles.Length)];
@@ -36,7 +30,7 @@ public class Wall : MonoBehaviour
                 instance.transform.SetParent(transform.parent);
             }
 
-        //Disable the gameObject.
-        gameObject.SetActive (false);
-	}
+            gameObject.SetActive(false);
+        }
+    }
 }
