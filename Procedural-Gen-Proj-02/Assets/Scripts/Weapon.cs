94042b05f2f39074c44f55ba97f63dd5aec46435
unknown <njrushak@protonmail.com>
2018-09-25 22:06:57 -0700

    building modular weapon and spawning

---
+++ Procedural-Gen-Proj-02/Assets/Scripts/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour
+{
+
+    public bool inPlayerInventory = false;
+
+    private Player player;
+    private WeaponComponents[] weaponsComps;
+    private bool weaponUsed = false;
+
+    public void AquireWeapon()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void useWeapon()
+    {
+
+    }
+
+    public void enableSpriteRender(bool isEnabled)
+    {
+
+    }
+
+    public Sprite getComponentImage(int index)
+    {
+        return null;
+    }
+}
